d962d889b6b80906dec44116e809687d271d5122
takubii <vicsat93@gmail.com>
2024-07-24 21:46:32 +0900

    add: TotalBalanceBoxコンポーネントを作成

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from '@/components/HeaderBox';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
   const loggedIn = { firstName: 'Adrian' };
@@ -13,6 +14,8 @@ const Home = () => {
             user={loggedIn?.firstName || 'Guest'}
             subtext='Access and manage your account and transactions efficiently.'
           />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
         </header>
       </div>
     </section>
