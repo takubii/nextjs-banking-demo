5571d966d1315215e1d37e014caa623be9fff2b4
takubii <vicsat93@gmail.com>
2024-08-01 21:52:31 +0900

    add: sign-upページ用のデザインを追加、typo修正

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -57,48 +57,54 @@ const AuthForm = ({ type }: { type: string }) => {
             <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-8'>
               {type === 'sign-up' && (
                 <>
-                  <CustomInput
-                    control={form.control}
-                    name='firstName'
-                    label='First name'
-                    placeholder='Enter your first name'
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name='lastName'
-                    label='Last name'
-                    placeholder='Enter your last name'
-                  />
+                  <div className='flex gap-4'>
+                    <CustomInput
+                      control={form.control}
+                      name='firstName'
+                      label='First name'
+                      placeholder='Enter your first name'
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name='lastName'
+                      label='Last name'
+                      placeholder='Enter your last name'
+                    />
+                  </div>
                   <CustomInput
                     control={form.control}
                     name='address1'
                     label='Address'
                     placeholder='Enter your specific address'
                   />
-                  <CustomInput
-                    control={form.control}
-                    name='state'
-                    label='State'
-                    placeholder='Example: NY'
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name='postalCode'
-                    label='Postal Code'
-                    placeholder='Exmaple: 11101'
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name='dateOfBirth'
-                    label='Date of Birth'
-                    placeholder='YYYY-MM-DD'
-                  />
-                  <CustomInput
-                    control={form.control}
-                    name='ssn'
-                    label='SSN'
-                    placeholder='Example: 1234'
-                  />
+                  <div className='flex gap-4'>
+                    <CustomInput
+                      control={form.control}
+                      name='state'
+                      label='State'
+                      placeholder='Example: NY'
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name='postalCode'
+                      label='Postal Code'
+                      placeholder='Exmaple: 11101'
+                    />
+                  </div>
+                  <div className='flex gap-4'>
+                    <CustomInput
+                      control={form.control}
+                      name='dateOfBirth'
+                      label='Date of Birth'
+                      placeholder='YYYY-MM-DD'
+                    />
+                    <CustomInput
+                      control={form.control}
+                      name='ssn'
+                      label='SSN'
+                      placeholder='Example: 1234'
+                    />
+                  </div>
                 </>
               )}
               <CustomInput
